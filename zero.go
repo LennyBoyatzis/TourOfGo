14e875448cce91b83c56656f8331f7a0408897f0
Lenny Boyatzis <lennyboyatzis@gmail.com>
2017-11-11 10:00:13 +1100

    Learning about zero vals

---
+++ zero.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// Variables declared without an explicit initial value
+// are given their zero value
+// Zero val for numeric types is 0
+// false for the boolean type
+// empty string for strings
+func main() {
+  var i int
+  var f float64
+  var b bool
+  var s string
+  fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
